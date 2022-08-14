21c2e9c63b19edde6388861d9dd7df37c1cc38f3
Flávio Eduardo <xflax00@gmail.com>
2022-08-13 22:31:06 -0300

    List

--- src/App.js
+++ src/App.js
@@ -1,44 +1,51 @@
 import './App.css';
-import { useState, useEffect } from 'react'
+import { useState,useEffect } from 'react'
+import List from './components/List/List'
 
 function App() {
 
   const [users, setUsers] = useState()
-  const [user, setUser] = useState()
-
 
   useEffect(() => {
-    fetch(`https://api.github.com/users/xFlax00/repos`)
-    .then(res => res.json())
-    .then(data => setUsers(data))
-  }, [])
+      fetch(`https://api.github.com/users/xFlax00/repos`)
+      .then(res => res.json())
+      .then(data => setUsers(data))
+      
+    }, [])
 
+  
+
+  /* function search(event){
+    event.preventDefault()
+    users && (
+      users.map((item) => {
+        return setRepos([...repos, item.name])
+      })
+    )
+  } */
+  const [filt, setFilt] = useState()
+
+  function filtred(){
+    console.log(filt)
+  }
 
   return (
     <div className="App">
       <section className="entradas">
 
         <div>
-          <input id="usuario" placeholder="Digite o nome do usuário" onChange={(e) => setUser(e.target.value)}></input>
+          <input id="usuario" placeholder="Digite o nome do usuário"></input>
           <button id="buscar">Buscar</button>
         </div>
 
         <div id="filtro">
-          <input placeholder="Digite o nome do repositório"></input>
-          <button>Filtrar</button>
+          <input placeholder="Digite o nome do repositório" onChange={(e) => setFilt(e.target.value)}></input>
+          <button onClick={filtred}>Filtrar</button>
         </div>
-
+      
       </section>
 
-      <section>
-      <ul className="repos">
-          {users && (
-            users.map((user) => {
-              return <li>{user.name}</li>
-            })
-          )}
-        </ul>
-      </section>
+      <List items={users}></List>
     </div>
   );
 }
---
+++ src/components/List/List.js
@@ -0,0 +1,15 @@
+function List(props){
+
+    return(
+            <ul className="repos">
+              {props.items && (
+                props.items.map((item) => {
+                    return <li key={item.name}>{item.name}</li>
+                })
+                
+                )}
+            </ul>
+    )
+}
+
+export default List
